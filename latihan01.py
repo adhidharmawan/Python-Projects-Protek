f6ba5580152bf4646220440f0774827876a95299
adhidharmawan <71803882+adhidharmawan@users.noreply.github.com>
2020-10-16 08:20:36 +1000

    Add files via upload

--- latihan01.py
+++ latihan01.py
@@ -1,1 +1,11 @@
-print("Hello world")
+#program hitung tarif rental
+
+tarif1 = 200000
+tarif2 = 10000
+jamMulai = 6
+menitMulai = 0
+jamSelesai = 23
+menitSelesai = 50
+lamaSewa = (jamSelesai - jamMulai) + (menitSelesai - menitMulai) / 60
+
+print(lamaSewa)
